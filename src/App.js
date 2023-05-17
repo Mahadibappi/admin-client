83597529b02146121f4c2ca51af2dd98b21d77d3
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-05-17 20:06:56 +0600

    theme added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import "./App.css";
+import { createTheme } from "@mui/material/styles";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      <h2>hello react</h2>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
